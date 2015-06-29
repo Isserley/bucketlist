b5047a1c47808b17f7d05a3e60d29b7437ed567b
Steve Thomas <gamgi1@hotmail.com>
2015-06-29 13:43:32 -0600

    generated controllers/views for entries and goals

---
+++ app/controllers/goals_controller.rb
@@ -0,0 +1,13 @@
+class GoalsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,22 @@
 Rails.application.routes.draw do
 
 
+  get 'entries/index'
+
+  get 'entries/new'
+
+  get 'entries/edit'
+
+  get 'entries/show'
+
+  get 'goals/index'
+
+  get 'goals/new'
+
+  get 'goals/edit'
+
+  get 'goals/show'
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
