87f87777dce543d6bc46848613728967631e2e37
Tyler Parker <tparker42d2@gmail.com>
2015-06-29 15:17:38 -0600

    incorporated simple layout and navbar. working on errors with username incorporation with devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:login]
+         :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => {email: true, login: false}
 
 
    # Virtual attribute for authenticating by either username or email
@@ -13,17 +13,18 @@ class User < ActiveRecord::Base
   # Overwrite Devise's find_for_database_authentication method to incorporate username into login process
   # Also configures the request for password change to allow username or email
 
-  before_create :create_login
-
-    def create_login
-      email = self.email.split(/@/)
-      login_taken = User.where(:login => email[0]).first
-      unless login_taken
-        self.login = email[0]
-      else
-        self.login = self.email
-      end
+  def self.find_first_by_auth_conditions(warden_conditions)
+  conditions = warden_conditions.dup
+  if login = conditions.delete(:login)
+    where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+  else
+    if conditions[:username].nil?
+      where(conditions).first
+    else
+      where(username: conditions[:username]).first
     end
+  end
+end
 
   #makes the username case insensitive
     validates :username,
