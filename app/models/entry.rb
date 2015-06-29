ccf4891067ee9d9989b80da637362e84a8db2ffb
Steve Thomas <gamgi1@hotmail.com>
2015-06-29 14:56:59 -0600

    index, new, edit, delete working for goal controller

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,5 @@
 class Entry < ActiveRecord::Base
   belongs_to :goal
+  has_attached_file :entry_img, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :entry_img, :content_type => /\Aimage\/.*\Z/
 end
