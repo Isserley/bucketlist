1028152d4d2adfe83710419c68bf0b3c8ca27ada
Tyler Parker <tparker42d2@gmail.com>
2015-06-29 14:34:00 -0600

    finished devise username configuration, moving on to layouts

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,22 +4,28 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:login]
 
+
    # Virtual attribute for authenticating by either username or email
    # This is in addition to a real persisted field like 'username'
   attr_accessor :login
 
 
   # Overwrite Devise's find_for_database_authentication method to incorporate username into login process
-  def self.find_for_database_authentication(warden_conditions)
-      conditions = warden_conditions.dup
-      if login = conditions.delete(:login)
-        where(conditions.to_hash).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+  # Also configures the request for password change to allow username or email
+
+  before_create :create_login
+
+    def create_login
+      email = self.email.split(/@/)
+      login_taken = User.where(:login => email[0]).first
+      unless login_taken
+        self.login = email[0]
       else
-        where(conditions.to_hash).first
+        self.login = self.email
       end
     end
 
-  #makes the username case insensitive  
+  #makes the username case insensitive
     validates :username,
   :presence => true,
   :uniqueness => {
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -5,6 +5,9 @@ Devise.setup do |config|
   config.authentication_keys = [ :login ]
 
   config.scoped_views = true
+
+  config.reset_password_keys = [ :username ]
+   config.confirmation_keys = [ :username ]
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
