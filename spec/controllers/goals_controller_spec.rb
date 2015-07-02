5fa2fded3b76871bc237691ac1cdc44d6cba61d1
Steve Thomas <gamgi1@hotmail.com>
2015-07-02 13:26:57 -0600

    testing on its way

--- spec/controllers/goals_controller_spec.rb
+++ spec/controllers/goals_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe GoalsController, type: :controller do
+  login_user
 
 let(:goal){FactoryGirl.create(:goal)}
 
@@ -8,6 +9,11 @@ let(:goal){FactoryGirl.create(:goal)}
     it "returns http success" do
       get :index
       expect(response).to have_http_status(:success)
+    end
+
+    it 'displays all goals' do
+      get :index
+      expect(Goal.all.count).to eq(Goal.all.count)
     end
   end
 
@@ -31,24 +37,29 @@ let(:goal){FactoryGirl.create(:goal)}
       expect(response).to have_http_status(:success)
     end
   end
+
   describe "PUT #update" do
-    it "returns http success" do
-      put :update, id: goal.id
+    it "updates a goal" do
+      put :update, id: goal.id, goal: {goal_title: 'Updated'}
       expect(response).to have_http_status(:redirect)
+      expect(goal.reload.goal_title).to eq('Updated')
     end
   end
 
-  # describe "POST #create" do
-  #   it "returns http success" do
-  #     post :create
-  #     expect(response).to have_http_status(:redirect)
-  #   end
-  # end
+  describe "POST #create" do
+      it "creates a goal" do
+        post :create, goal: {goal_title: 'Test'}
+        expect(response).to have_http_status(:redirect)
+        expect(Goal.last.goal_title).to eq('Test')
+      end
+    end
 
-#    describe "DELETE #destroy" do
-#     it "returns http success" do
-#       delete :destroy, id: entry.id
-#       expect(response).to have_http_status(:redirect)
-#     end
-#   end
+    describe "DELETE #destroy" do
+        it "deletes a goal" do
+          delete :destroy, id: goal.id
+          expect(response).to have_http_status(:redirect)
+          expect(Goal.all.count).to eq(Goal.all.count)
+        end
+      end
+
 end
